dba7c044199ebe57409832bd2031a61d38156041
Petr Sysel <petrsysel99@gmail.com>
2023-12-07 06:52:05 +0100

    Vytvořena instrance hlavní třídy. Zatím s neimplementovanými závislostmi

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
 function main(){
-    console.log("App started...")
+    const localStorage = {} as IWorkspaceRepository
+    const externalStorage = {} as IWorkspaceRepository
+    const controlPanel = {} as IControlPanelUI
+    const confirmDialogue = {} as IDialogueUI<boolean>
+    const inputDialogue = {} as IDialogueUI<string>
+    const richTextDialogue = {} as IDialogueUI<string>
+    const colorDialogue = {} as IDialogueUI<NoteColor>
+    const board = {} as IBoardUI
+
+    const app = new App(
+        localStorage,
+        externalStorage,
+        controlPanel,
+        confirmDialogue,
+        inputDialogue,
+        richTextDialogue,
+        colorDialogue,
+        board
+    )
 } 

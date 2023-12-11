df4157087ca7d74b4942adb2b5b01a1dfb6944d2
Petr Sysel <petrsysel99@gmail.com>
2023-12-11 08:27:14 +0100

    Vytvořeno dialogové okno pro vstup od uživatele

---
+++ src/adapters/InputDialogue.ts
@@ -0,0 +1,39 @@
+class InputDialogue implements IDialogueUI<string>{
+    private dialogueElement: HTMLElement
+    private okButtonElement: HTMLElement
+    private closeButtonElement: HTMLElement
+    private inputElement: HTMLInputElement
+
+    constructor(){
+        this.dialogueElement = document.createElement("div")
+        this.dialogueElement.innerHTML = AjaxLoader.load("./templates/InputDialogue.html")
+        const body = DOMHelper.getBody()
+        body.appendChild(this.dialogueElement)
+
+        this.okButtonElement = DOMHelper.get("input-dialogue-ok-btn")
+        this.closeButtonElement = DOMHelper.get("input-dialogue-close-btn")
+        this.inputElement = DOMHelper.get("dialogue-user-input") as HTMLInputElement
+
+        this.hide()
+    }
+    on(event: "data-change", callback: (data: string) => void): void {
+        
+    }
+    show(): Promise<string|DialogueCanceled> {
+        this.dialogueElement.style.display = "block"
+        return new Promise((resolve, reject) => {
+            this.okButtonElement.addEventListener("click", () => {
+                this.hide()
+                resolve(this.inputElement.value)
+            })
+            this.closeButtonElement.addEventListener("click", () => {
+                this.hide()
+                resolve(undefined)
+            })
+        })
+    }
+
+    private hide(){
+        this.dialogueElement.style.display = "none"
+    }
+}
--- src/ports/IDialogueUI.ts
+++ src/ports/IDialogueUI.ts
@@ -1,6 +1,7 @@
 type DialogueEvent = "data-change"
+type DialogueCanceled = undefined
 
 interface IDialogueUI<T> {
     on(event: DialogueEvent, callback: (data: T) => void): void
-    show(): Promise<T>
+    show(): Promise<T|DialogueCanceled>
 }
